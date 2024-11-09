17c7a76642a6b24b61d5258666681b1311b59f87
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2024-11-09 01:48:33 -0300

    test: aplication repliet api

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+const endpoint = "https://bba1a9a5-e22e-42dd-bc80-f2423b87a335-00-q5bu0ovtpf5l.riker.replit.dev/"
+fetch(endpoint)
+.then(res=>console.log(res))
+// .then(dados=>{
+//     console.log(dados)
+// })
+
