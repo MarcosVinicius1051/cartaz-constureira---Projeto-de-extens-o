9a1b3eac5b3decdafbc747170bef2eb67021eff2
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2024-11-14 19:31:00 -0300

    incomplet feat: add sistem tags

--- JS/functionsForSistem.js
+++ JS/functionsForSistem.js
@@ -64,8 +64,9 @@ function createItemsInContainers(
   indexArrName
 ) {
   let elementDivCreat = document.createElement("div");
-  elementDivCreat.id = `${id} ${category}`;
+  elementDivCreat.id = `${id}`;
   elementDivCreat.classList.add("containerElement");
+  elementDivCreat.classList.add(category);
   let imgElement = document.createElement("img");
   imgElement.src = img;
   imgElement.alt = `${title} img`;
@@ -157,4 +158,50 @@ export function clickContainerElements(elementsDiv, p) {
     elementsDiv.classList.add("selected"); // adiciona a classe de selected
     p.classList.remove("hide");
   }
+}
+
+//Tags salactions
+
+export function selectTags(tags, elements) {
+  let arr;
+  switch (tags.classList[1]) {
+    case "tagBeauty":
+      selectTagsActions(
+        1,
+        selectElementsForTags(elements, "tagBeauty"),
+        elements
+      );
+      break;
+    case "tagFragrances":
+      break;
+    case "tagFurniture":
+      break;
+    case "tagsGroceries":
+      break;
+  }
+}
+
+function selectTagsActions(key, elActived, el) {
+  el.map((el) => {
+    if (el.classList[1] == "beauty") {
+      if(el.parentNode.className.endsWith("hide")){
+        el.parentNode.classList.remove("hide");
+      }
+    } else {
+      el.parentNode.classList.add("hide");
+    }
+  });
+}
+
+function selectElementsForTags(el, nameTag) {
+  let arr = [];
+  el.map((el) => {
+    if (nameTag == "tagBeauty") {
+      if (el.classList[1] == "beauty") {
+        arr.push(el);
+      }
+    }
+  });
+
+  return arr;
 }
--- JS/index.js
+++ JS/index.js
@@ -5,43 +5,49 @@ export let result = fetch(url)
   .then((res) => res.json())
   .then((res) => res.products);
 
-let containerElement ;
+let containerElement;
 let containerDivElements;
-let containerPElements
-let resultProducts =  await fun.products()
+let containerPElements;
+let containerTagsElementsButtons;
+let resultProducts = await fun.products();
 
-
-//sistema de pegar os elementos DEPOIS de serem puxados do DOM 
-if(resultProducts != false){
+//sistema de pegar os elementos DEPOIS de serem puxados do DOM
+if (resultProducts != false) {
   containerElement = [...document.querySelectorAll(".containerElement")];
-  containerDivElements = [...document.querySelectorAll(".containerDivElements")];
-  containerPElements = [...document.querySelectorAll(".descriptionElement")]
+  containerDivElements = [
+    ...document.querySelectorAll(".containerDivElements"),
+  ];
+  containerPElements = [...document.querySelectorAll(".descriptionElement")];
+  containerTagsElementsButtons = [...document.querySelectorAll(".btnTags")];
 }
 
-
-
-
-
-fun.btnNext.map((btnNext)=>{
-  btnNext.addEventListener("click",(evt)=>{
+fun.btnNext.map((btnNext) => {
+  btnNext.addEventListener("click", (evt) => {
     fun.nextElements(containerDivElements);
   });
-})
-fun.btnBack.map((btnBack)=>{
-  btnBack.classList.add("hide")
-  btnBack.addEventListener("click",(evt)=>{
+});
+fun.btnBack.map((btnBack) => {
+  btnBack.classList.add("hide");
+  btnBack.addEventListener("click", (evt) => {
     fun.backElements(containerDivElements);
   });
 });
 
-containerElement.map((containerElement)=>{
-  containerElement.addEventListener("click", (evt)=>{
-    // ao clicar class hide de "p" é retirada. 
-    fun.clickContainerElements(containerElement,containerElement.lastElementChild)//pega o ultimo elemento filho, ou seja "p"
+containerElement.map((containerElement) => {
+  containerElement.addEventListener("click", (evt) => {
+    // ao clicar class hide de "p" é retirada.
+    fun.clickContainerElements(
+      containerElement,
+      containerElement.lastElementChild
+    ); //pega o ultimo elemento filho, ou seja "p"
+  });
+});
 
-  })
-})
+containerTagsElementsButtons.map((containerTagsElementsButtons) => {
+  containerTagsElementsButtons.addEventListener("click", (evt) => {
 
-
+    fun.selectTags(containerTagsElementsButtons,containerElement);
+  });
+});
 
 
